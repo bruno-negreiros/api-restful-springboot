fdfb68932d37e7895417e9726541a988eee1f599
Bruno Negreiros <bruno.ananias_n@hotmail.com>
2020-01-16 17:58:47 -0400

    Get carro by id.

--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarroService {
@@ -24,5 +25,9 @@ public class CarroService {
         carros.add(new Carro(3L, "Chevette"));
 
         return carros;
+    }
+
+    public Optional<Carro> getCarroById(Long id) {
+        return rep.findById(id);
     }
 }
