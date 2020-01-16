8c12218962c7a2f7ecc15e902ddc68cd152bdac9
Bruno Negreiros <bruno.ananias_n@hotmail.com>
2020-01-16 15:37:41 -0400

    Lista de carros básica.

--- src/main/java/com/example/carros/api/IndexController.java
+++ src/main/java/com/example/carros/api/IndexController.java
@@ -8,24 +8,7 @@ public class IndexController {
 
     @GetMapping
     public String get() {
-        return "Get Spring Boot";
-    }
-
-    @GetMapping("login")
-    public String post(@RequestParam("login") String login, @RequestParam("senha") String senha) {
-        return "Login: " + login + "/ Senha: " + senha + ".";
-    }
-
-    // POST - RequestParam
-    @PostMapping("login")
-    public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
-        return "Login: " + login + "/ Senha: " + senha + ".";
-    }
-
-    // @PathVariable
-    @GetMapping("login/{login}/senha/{senha}")
-    public String post2(@PathVariable("login") String login, @PathVariable("senha") String senha) {
-        return "Login: " + login + "/ Senha: " + senha + ".";
+        return "API dos Carros";
     }
 
 }
