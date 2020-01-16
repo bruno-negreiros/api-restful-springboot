98fcffbf3119e7c960c1c28a82b9b9be93a70160
Bruno Negreiros <bruno.ananias_n@hotmail.com>
2020-01-16 17:49:33 -0400

    JPA - Lista de carros do banco de dados.

--- src/main/java/com/example/carros/domain/Carro.java
+++ src/main/java/com/example/carros/domain/Carro.java
@@ -1,8 +1,20 @@
 package com.example.carros.domain;
 
+import javax.persistence.*;
+
+// @Entity(name = "carro") - Caso objeto com o nome diferente da tabela.
+@Entity
 public class Carro {
+
+    @Id // Chave Primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autoincremento
     private Long id;
+
+//    @Column(name = "nome") - Caso propriedade com o nome diferente da coluna da tabela.
     private String nome;
+
+
+    public Carro() {}
 
     public Carro(Long id, String nome) {
         this.id = id;
