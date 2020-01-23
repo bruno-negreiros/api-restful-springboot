a27615aab2bbac7f4d81c21f8c15264ddb80abe2
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2020-01-23 01:55:27 -0400

    POST - Salvar carro.

--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -35,4 +35,8 @@ public class CarroService {
     public Iterable<Carro> getCarrosByTipo(String tipo) {
         return rep.findByTipo(tipo);
     }
+
+    public Carro save(Carro carro) {
+        return rep.save(carro);
+    }
 }
