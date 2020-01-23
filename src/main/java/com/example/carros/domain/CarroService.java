951a8dba5e1da5a27472f9b3636fe7334234bf3f
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2020-01-23 02:25:31 -0400

    PUT - Atualizar o carro.

--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -2,6 +2,7 @@ package com.example.carros.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import javax.swing.text.html.Option;
 import java.util.ArrayList;
@@ -36,7 +37,46 @@ public class CarroService {
         return rep.findByTipo(tipo);
     }
 
-    public Carro save(Carro carro) {
+    public Carro insert(Carro carro) {
+        Assert.isNull(carro.getId(), "Não foi possível inserir o registro");
         return rep.save(carro);
     }
+
+    public Carro update(Carro carro, Long id) {
+        Assert.isNull(carro.getId(), "Não foi possível atualizar o registro");
+
+        // Busca o carro no banco de dados
+        Optional<Carro> optional = getCarroById(id);
+
+        if (optional.isPresent()) {
+            Carro db = optional.get();
+
+            // Copiar as propriedades
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+
+            // Atualiza o carro
+            rep.save(db);
+
+            return db;
+        } else {
+            throw new RuntimeException("Não foi possível atualizar o registro");
+        }
+
+        /* Lambda
+        getCarroById(id).map(db -> {
+            // Copiar as propriedades
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+
+            // Atualiza o carro
+            rep.save(db);
+
+            return db;
+        }).orElseThrow(() -> new RuntimeException("Não foi possível atualizar o registro"));
+         */
+
+    }
+
+
 }
