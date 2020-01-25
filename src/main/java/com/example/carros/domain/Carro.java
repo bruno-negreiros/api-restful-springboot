6fc81143b6012e55426589b6f46bd2096002933c
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2020-01-25 15:15:38 -0400

    Lombok

--- src/main/java/com/example/carros/domain/Carro.java
+++ src/main/java/com/example/carros/domain/Carro.java
@@ -1,48 +1,21 @@
 package com.example.carros.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
-// @Entity(name = "carro") - Caso objeto com o nome diferente da tabela.
-@Entity
+
+@Entity // @Entity(name = "carro") - Caso objeto com o nome diferente da tabela.
+@Data // equivale ao @Getter, @Setter, @ToString e @EqualsAndHashCode.
+@NoArgsConstructor
+@AllArgsConstructor
 public class Carro {
 
     @Id // Chave Primária
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autoincremento
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autoincremento.
     private Long id;
 
-//    @Column(name = "nome") - Caso propriedade com o nome diferente da coluna da tabela.
-    private String nome;
+    private String nome; //    @Column(name = "nome") - Caso propriedade com o nome diferente da coluna da tabela.
     private String tipo;
 
-
-    public Carro() {}
-
-    public Carro(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 }
