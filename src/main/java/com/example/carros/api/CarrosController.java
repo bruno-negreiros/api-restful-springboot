e5c924d572af64902fb61eee1e1bb412331d4fe9
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2020-01-25 14:50:01 -0400

    ResponseEntity

--- src/main/java/com/example/carros/api/CarrosController.java
+++ src/main/java/com/example/carros/api/CarrosController.java
@@ -3,6 +3,8 @@ package com.example.carros.api;
 import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
@@ -17,8 +19,9 @@ public class CarrosController {
     private CarroService service;
 
     @GetMapping
-    public Iterable<Carro> get() {
-        return service.getCarros();
+    public ResponseEntity<Iterable<Carro>> get() {
+        return ResponseEntity.ok(service.getCarros());
+//        return new ResponseEntity<>(service.getCarros(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
